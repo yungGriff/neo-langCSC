ca24b5bfcd4c04f13011a1028ae119248ba66334
yungGriff <57887563+yungGriff@users.noreply.github.com>
2022-11-16 20:11:31 -0600

    HW15

--- main.rkt
+++ main.rkt
@@ -10,7 +10,7 @@
 (define sample-code3 '(call (function (r) (local-vars ((p c)) (math / r p)) ) (a)))
 ;(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
 (define sample-code2 '(call (function (a) (call (function (r) a) (a))) (5)))
-
+;1
 (define sample-code '(print a))
 (displayln (neo-parser sample-code))
 (define parsed-neo-code (neo-parser sample-code))
